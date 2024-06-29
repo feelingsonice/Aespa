ed7ebd1eb30c6f889614d7103014fef8280ae769
B <42384776+feelingsonice@users.noreply.github.com>
2024-06-29 01:51:46 -0700

    camera selection

--- Sources/Aespa/Util/Extension/AVFoundation+Extension.swift
+++ Sources/Aespa/Util/Extension/AVFoundation+Extension.swift
@@ -1,6 +1,6 @@
 //
 //  AVFoundation + Extension.swift
-//  
+//
 //
 //  Created by Young Bin on 2023/05/28.
 //
@@ -9,23 +9,16 @@ import AVFoundation
 
 extension AVCaptureDevice.Position {
     var chooseBestCamera: AVCaptureDevice? {
-        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera,
-                                                                              .builtInTripleCamera,
-                                                                              .builtInWideAngleCamera],
-                                                                mediaType: .video,
-                                                                position: self)
-        
-        // Sort the devices by resolution
-        let sortedDevices = discoverySession.devices.sorted { (device1, device2) -> Bool in
-            guard let maxResolution1 = device1.maxResolution,
-                  let maxResolution2 = device2.maxResolution else {
-                return false
-            }
-            return maxResolution1 > maxResolution2
-        }
-
-        // Return the device with the highest resolution, or nil if no devices were found
-        return sortedDevices.first
+        AVCaptureDevice.DiscoverySession(
+            deviceTypes: [
+                .builtInTripleCamera,
+                .builtInDualWideCamera,
+                .builtInDualCamera,
+                .builtInWideAngleCamera
+            ],
+            mediaType: .video,
+            position: self
+        ).devices.first
     }
 }
 
